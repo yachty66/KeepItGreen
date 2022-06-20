36207fee77e3ca98b5a263751a6e0dd1c691ab1c
yachty66 <68147541+yachty66@users.noreply.github.com>
2022-06-20 20:47:20 +0200

    comment from python script

--- PushMe.py
+++ PushMe.py
@@ -1,9 +1,17 @@
 #I get pushed regularly!
 import random
 
+#1. create txt file 
+#2. write a function which writes into this file when called
+#3. run script and run autopush to see if it works
+
 def pushMe():
     #create rare number for 
     randomNumber = random.randint(0, 100000) + random.randint(0, 100000) - random.randint(0, 100000) * random.randint(0, 100000) / random.randint(0, 100000)
-    return randomNumber 
+    return str(randomNumber)
 
-pushMe()
+def writeToFile():
+    with open("Change.txt", "w") as textFile:
+        textFile.write(pushMe())
+
+writeToFile()
