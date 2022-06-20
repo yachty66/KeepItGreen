30ba49c68ab0ffa44b395602083212ab4b0ecccd
yachty66 <68147541+yachty66@users.noreply.github.com>
2022-06-20 20:27:48 +0200

    e

---
+++ Auto.py
@@ -0,0 +1,19 @@
+from git import Repo
+
+PATH_OF_GIT_REPO = r"/Users/maxhager/Projects/KeepItGreen"  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'comment from python script'
+
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        print("ssss")
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        print("ssss")
+        origin = repo.remote(name='origin')
+        print("ssss")
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')    
+
+git_push()
---
+++ WriteToFile.py
@@ -0,0 +1,9 @@
+#I get pushed regularly!
+import random
+
+def pushMe():
+    #create rare number for 
+    randomNumber = random.randint(0, 100000) + random.randint(0, 100000) - random.randint(0, 100000) * random.randint(0, 100000) / random.randint(0, 100000)
+    return randomNumber 
+
+print(pushMe())
